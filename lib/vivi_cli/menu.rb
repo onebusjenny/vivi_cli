22c9cd2fef5acf6cffe9604675dac6bb12112892
Jenny Chu <jchudesign0930@gmail.com>
2019-04-15 04:49:57 +0000

    menu

--- lib/vivi_cli/menu.rb
+++ lib/vivi_cli/menu.rb
@@ -1,5 +1,20 @@
-class Menu
+require 'nokogiri'
+requite 'open-uri'
+
+
+module VIVI
+  class Menu
    attr_accessor :Vivi_Special, :Flavored_Tea, :Milk_Tea, :Fresh_Fruit, :Slush, :Float_Yakult, :Fresh_Milk
+   
+   def self.list_names
+     self.scrape_list
+   end
+   
+   def self.scrape_list
+     menu_list = []
+     doc = Nokogiri::HTML(open(http://www.vivibubbletea.com/index.php))
+     menu = doc.search('div.hone_menu03')
+   end
    
    
 end
