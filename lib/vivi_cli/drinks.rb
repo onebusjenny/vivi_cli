48b9c72980b2195923fcb7bf4a8472bec9f13b82
Jenny Chu <jchudesign0930@gmail.com>
2019-04-15 04:01:35 +0000

    drinks

--- lib/vivi_cli/drinks.rb
+++ lib/vivi_cli/drinks.rb
@@ -3,7 +3,19 @@ class Drinks::CLI
   
   def call 
     puts "What would you like to drink today?"
-   
+    menu
+  end
+  
+  def menu
+    puts "Enter the drink category name you want:"
+    input = gets.strip
+    case input
+    when "Milk Tea"
+      puts @milk_list
+    when "Tea"
+      puts @tea_list
+    when "Fruit"
+      puts @fruit_list
   end
   
 end
