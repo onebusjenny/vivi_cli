9cd97c3e6c60ed09f16c0b1d46d40346f22d3778
Jenny Chu <jchudesign0930@gmail.com>
2019-04-15 04:20:10 +0000

    list

--- lib/vivi_cli/drinks.rb
+++ lib/vivi_cli/drinks.rb
@@ -3,12 +3,16 @@ class Drinks::CLI
   
   def call 
     puts "What would you like to drink today?"
+     puts @category_list
     menu
   end
   
   def menu
-    puts "Enter the drink category name you want:"
-    input = gets.strip
+
+   input = nil
+   while input != "exit"
+   puts "Enter the drink category name you want or type menu to see the list again or type exit to leave:"
+    input = gets.strip.downcase
     case input
     when "Milk Tea"
       puts @milk_list
@@ -16,6 +20,16 @@ class Drinks::CLI
       puts @tea_list
     when "Fruit"
       puts @fruit_list
+    when "menu"
+      menu
+    else
+      puts "Not sure what you want, try it, type list or exit"
   end
-  
+  end
+end
+
+def thankyou
+  puts "thank you for your order!"
+end
+
 end
